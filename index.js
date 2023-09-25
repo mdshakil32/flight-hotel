8fac3f2b81adf66a017101a7df0d54cfecf6fb41
abudaudhossain <abudaudhossainsumon@gmail.com>
2023-09-25 16:07:14 +0600

    :zap: add cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require("cors")
 const flightList = require("./flight")
 const hotelList = require("./hotelList")
 
 const app = express();
+app.use(cors());
 // app.use(express.static('public'));
 app.use((req, res, next) => {
     req.rootDir = __dirname;
